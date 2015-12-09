b3426a8c0dfe03cab2e422e85297dff0c81242ae
Alexander Karaberov <alexander.karaberov@gmail.com>
2015-12-09 02:02:10 +0200

    Add comment

--- app/src/main/java/com/example/alexander/bluetoothprinting/activities/MainActivity.java
+++ app/src/main/java/com/example/alexander/bluetoothprinting/activities/MainActivity.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBluetoothAdapter;
@@ -34,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
+    //Print button's onClick event
     public void printBarcode(View v) {
 
         final EditText barcodeTextEdit = (EditText)findViewById(R.id.editText);
